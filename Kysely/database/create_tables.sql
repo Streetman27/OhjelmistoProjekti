6982b97d3608d01bdd55997e3470a4064c1c770b
Brygger Kim <a1600587@myy.haaga-helia.fi>
2017-04-21 12:25:53 +0300

    sql to finnish

--- Kysely/database/create_tables.sql
+++ Kysely/database/create_tables.sql
@@ -1,47 +1,26 @@
---POLL TABLES
-CREATE TABLE QUESTION_TYPE
+--KYSELY TAULUT
+CREATE TABLE kysymys_tyyppi
 (id integer NOT NULL auto_increment PRIMARY KEY
-,definition varchar(255) NOT NULL
+,maaritelma varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE POLL
+CREATE TABLE kysely
 (id integer NOT NULL auto_increment PRIMARY KEY
-,name varchar(255) NOT NULL
+,nimi varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE QUESTION
+CREATE TABLE kysymys
 (id integer NOT NULL auto_increment PRIMARY KEY
-,question varchar(255) NOT NULL
-,questionType_id integer NOT NULL
+,kysmys varchar(255) NOT NULL
+,tyyppi_id integer NOT NULL
 ,poll_id integer NOT NULL
-,FOREIGN KEY (questionType_id) REFERENCES QUESTION_TYPE(id)
-,FOREIGN KEY (poll_id) REFERENCES POLL(id)
+,FOREIGN KEY (tyyppi_id) REFERENCES kysely_tyyppi(id)
+,FOREIGN KEY (kysely_id) REFERENCES kysely(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE ANSWER
+CREATE TABLE vastaus
 (id integer NOT NULL auto_increment PRIMARY KEY
-,text varchar(255) NOT NULL
-,question_id integer NOT NULL
-,FOREIGN KEY (question_id) REFERENCES QUESTION(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---USER TABLES
-CREATE TABLE authority
-(id integer NOT NULL auto_increment PRIMARY KEY
-,role varchar(255) NOT NULL UNIQUE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
- 
-CREATE TABLE webuser 
-(id integer NOT NULL auto_increment PRIMARY KEY
-,username varchar(255) NOT NULL UNIQUE
-,password_encrypted varchar(255) NOT NULL
-,enabled tinyint NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE webuser_authority 
-(id integer NOT NULL auto_increment PRIMARY KEY
-,webuser_id integer NOT NULL
-,authority_id integer NOT NULL
-,FOREIGN KEY (webuser_id) REFERENCES webuser(id) ON DELETE NO ACTION ON UPDATE NO ACTION
-,FOREIGN KEY (authority_id) REFERENCES authority(id) ON DELETE NO ACTION ON UPDATE NO ACTION
+,teksti varchar(255) NOT NULL
+,kysymys_id integer NOT NULL
+,FOREIGN KEY (kysymys_id) REFERENCES kysymys(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- Kysely/database/insert_testdata.sql
+++ Kysely/database/insert_testdata.sql
@@ -1,27 +1,28 @@
-INSERT INTO QUESTION_TYPE
-	(id, definition)
+--KYSELY TAULUT
+INSERT INTO kysymys_tyyppi
+	(id, maaritelma)
 values
-	(1, 'choice'),
+	(1, 'vaihtoehto'),
 	(2, 'radio'),
 	(3, 'checkbox'),
 	(4, 'textarea');
 
-INSERT INTO POLL
-	(id, name)
+INSERT INTO kysely
+	(id, nimi)
 values
 	(1, 'Testikysely1'),
 	(2, 'Testikysely2');
 
-INSERT INTO QUESTION
-	(id, question, questionType_id, poll_id)
+INSERT INTO kysymys
+	(id, kysymys, tyyppi_id, kysely_id)
 values
 	(1, 'Onko tämä testikysymys?', 3, 1),
 	(2, 'Onko kakku hyvää?', 1, 1),
 	(3, 'Oletko viimeisen kuukauden aikana syönyt meetvurstia?', 2, 2),
 	(4, 'Oletko viime aikoina tuntenut olosi ihmiseksi?', 4, 2);
 
-INSERT INTO ANSWER
-	(text, question_id)
+INSERT INTO vastaus
+	(teksti, kysymys_id)
 values
 	('kyllä', 1),
 	('ei', 1),
@@ -29,22 +30,3 @@ values
 	('vähän', 3),
 	('sopivasti', 3),
 	('paljon', 3);
-
---USER TABLES
-INSERT INTO authority
-	(id, role)
-VALUES
-	(2,'ROLE_ADMIN'),
-	(1,'ROLE_USER');
-
-INSERT INTO webuser
-	(id, username, password_encrypted, enabled)
-VALUES
-	(1,'admin','00',1),
-	(2,'user','00',1);
-
-INSERT INTO webuser_authority
-	(id, webuser_id, authority_id)
-VALUES
-	(1,1,2),
-	(2,2,1);
