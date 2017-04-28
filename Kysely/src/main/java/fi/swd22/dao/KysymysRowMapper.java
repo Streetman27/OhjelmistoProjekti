75b60505c22d3cc4e5f890127a423520112b539c
Tatu Korpela <tatu.korpela@netti.fi>
2017-04-28 14:34:03 +0300

    Turhia poistettu

--- Kysely/src/main/java/fi/swd22/dao/KysymysRowMapper.java
+++ Kysely/src/main/java/fi/swd22/dao/KysymysRowMapper.java
@@ -2,16 +2,11 @@ package fi.swd22.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.jdbc.core.RowMapper;
 
 import fi.swd22.bean.Kysymys;
 import fi.swd22.bean.KysymysImpl;
-import fi.swd22.bean.Vastaus;
-import fi.swd22.bean.VastausImpl;
 
 public class KysymysRowMapper implements RowMapper<Kysymys> {
 
@@ -20,23 +15,6 @@ public class KysymysRowMapper implements RowMapper<Kysymys> {
 		kysymys.setId(rs.getInt("id"));
 		kysymys.setKysymys(rs.getString("kysymys"));
 		kysymys.setTyyppi(rs.getString("maaritelma"));
-		
-		
-		//vastaus arvot
-		String arvot = (rs.getString("arvot")); //Esimerkiksi splittaa Kyllä,Ei,Ehkä
-		List<String> items = Arrays.asList(arvot.split("\\s*,\\s*"));
-		List<Vastaus> vastaukset = new ArrayList<Vastaus>();
-
-		
-//		for (String arvo : items) {
-		for (int i = 0; i < items.size(); i++) {
-			
-			Vastaus vastaus = new VastausImpl();
-			vastaus.setArvo(items.get(i));
-			vastaukset.add(vastaus);
-		}
-
-		kysymys.setVastaukset(vastaukset);
 		
 		
 		return kysymys;
